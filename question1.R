0092eedc45680cf17c9d60e69addd1065ba03716
bschneeman <bschneeman@homeaway.com>
2014-10-06 20:29:31 -0500

    updated to answer question. using dplyr!

--- question1.R
+++ question1.R
@@ -1,11 +1,23 @@
 # Exploratory Data Analysis Programming Assignment #2
 
+require(dplyr)
+
 # read the files
-files = dir("data")
-if(sum(grepl("summarySCC_PM25.rds", files))>0){
-  NEI <- readRDS("data/summarySCC_PM25.rds")
+
+if(sum(grepl("NEI", ls())) == 0){
+  if(file.exists("data/summarySCC_PM25.rds")){
+    NEI <- readRDS("data/summarySCC_PM25.rds")
+  }
 }
-if(sum(grepl("Source_Classification_Code.rds", files))>0){
-  SCC <- readRDS("data/Source_Classification_Code.rds")
+if(sum(grepl("SCC", ls())) == 0){
+  if(file.exists("data/Source_Classification_Code.rds")){
+    SCC <- readRDS("data/Source_Classification_Code.rds")
+  }
 }
 
+nei_sum = NEI %>% group_by(year) %>% summarize(totalEmissions = sum(Emissions))
+
+png(file="q1.png")
+plot(nei_sum$year, nei_sum$totalEmissions, type="l")
+points(nei_sum$year, nei_sum$totalEmissions)
+dev.off()
